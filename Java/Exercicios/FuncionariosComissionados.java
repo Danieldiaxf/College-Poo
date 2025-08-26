3279c6a7bbfed91e86b4149d2e9a89f986d724d9
Daniel à D Fernandes <danieldias@dinfor.uniceub>
2025-08-25 21:13:51 -0300

    Empresa.java

--- Java/Exercicios/FuncionariosComissionados.java
+++ Java/Exercicios/FuncionariosComissionados.java
@@ -2,14 +2,25 @@ package Exercicios;
 
 public class FuncionariosComissionados extends Funcionarios {
     
-
-
     public double comissao;
-    
-    public double calcularSalario(double x) {
-        
-        return calcularSalario( x );
+
+    public double SalarioC( double x ){
+
+        this.comissao = x;
+
+        return x;
+
+
     }
+
+    public double setSalarioC( double comissao ){
+
+        this.comissao = comissao;
+
+        return ( comissao * getSalarioB() );
+        
+    }
+
 
 
 }

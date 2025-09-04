722b93badfc07c55f15aea2dde5db3463e7d7ce8
Daniel Dias <danieldias0312@gmail.com>
2025-09-04 13:43:31 -0300

    Before Constructor

---
+++ Introdução à poo/Static Members/Constructor/Product.java
@@ -0,0 +1,29 @@
+package Constructor;
+
+public class Product {
+    
+    public String name;
+    public double price;
+    public int quantity;
+
+    public int addProducts( int value ){
+
+
+        return quantity + value;
+
+
+    }
+
+
+        public int removeProducts( int value ){
+
+
+        return quantity + value;
+
+
+    }
+
+
+
+
+}

1c9ce04a0bbb118d529385843f5cc6bae7648689
Daniel Dias <danieldias0312@gmail.com>
2025-09-04 10:01:38 -0300

    Some Changes

--- Introdução à poo/Static Members/Exemple01.java
+++ Introdução à poo/Static Members/Exemple01.java
@@ -7,19 +7,18 @@ public class Exemple01 {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner( System.in );
-        Calculator calc = new Calculator();
 
         System.out.print( "Enter the radius:" );
         double radius = sc.nextDouble(  );
 
-        double c = calc.circumference( radius );
+        double c = Calculator.circumference( radius );
 
-        double v = calc.volume( radius );
+        double v = Calculator.volume( radius );
 
 
         System.out.printf( "The Circumference's value: %.2f", c );
         System.out.printf( "\nThe Volume's value : %.2f", v );
-        System.out.printf( "\nThe PI has value: %.2f", calc.PI );
+        System.out.printf( "\nThe PI has value: %.2f", Calculator.PI );
 
         sc.close();
 

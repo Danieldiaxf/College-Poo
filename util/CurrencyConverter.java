1f102e7f26a504d8b427543c90fae4bb9fa0c233
Daniel Dias <danieldias0312@gmail.com>
2025-09-04 10:29:55 -0300

    Exercise 2

---
+++ util/CurrencyConverter.java
@@ -0,0 +1,14 @@
+package util;
+
+public class CurrencyConverter {
+    
+    public static final double IOF = 0.06;
+
+    public static double calc( double sum ){
+
+        return ( sum * IOF )  + ( sum );
+        
+    }
+
+
+}

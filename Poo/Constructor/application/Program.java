a844c592081690bd435a35828cd7d7516e5c84fb
Daniel Dias <danieldias0312@gmail.com>
2025-10-21 15:26:07 -0300

    Access Modification

--- Poo/Constructor/application/Program.java
+++ Poo/Constructor/application/Program.java
@@ -11,21 +11,19 @@ public class Program{
 
     System.out.println( "Enter product data: " );
     System.out.print( "Name:" );
+
     String name = sc.nextLine(  );
     
     System.out.print( "Price:" );
     double price = sc.nextDouble();
     
-    System.out.print( "Quantity in Stock: " );
-    int quantity = sc.nextInt(  );
-
-    Product product = new Product( "TV", 1000, 3 );
+    Product product = new Product( name, price );
 
 
     System.out.println( product.toString(  ) );
     System.out.print( "Enter the number of products to be addes in stock: " );
 
-    quantity = sc.nextInt(  );
+    int quantity = sc.nextInt(  );
     product.addProducts( quantity );
    
     System.out.println( product.toString(  ) );
--- Poo/Constructor/entities/Product.java
+++ Poo/Constructor/entities/Product.java
@@ -6,11 +6,19 @@ public class Product {
     public double price;
     public int quantity;
 
+    public Product(){
+        
+    }
 
     public Product( String name, double price, int quantity ){
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+    }
+
+    public Product( String name, double price ){
+        this.name = name;
+        this.price = price;
     }
 
 
@@ -25,7 +33,6 @@ public class Product {
     public void removeProducts( int quantity ){
         this.quantity -= quantity;
     }
-
 
     public String toSring( ){
         return "Product Data: " + name + 

53bbafe67ed05d009e73e3b8c70afdc4da562713
Daniel Dias <danieldias0312@gmail.com>
2025-10-21 10:01:28 -0300

    bug fixes

---
+++ Poo/Constructor/application/Program.java
@@ -0,0 +1,46 @@
+package Poo.Constructor.application;
+import java.util.Scanner;
+
+import Poo.Constructor.entities.Product;
+
+public class Program{
+
+    public static void main( String[ ] args ){
+
+    Scanner sc = new Scanner( System.in );
+
+    System.out.println( "Enter product data: " );
+    System.out.print( "Name:" );
+    String name = sc.nextLine(  );
+    
+    System.out.print( "Price:" );
+    double price = sc.nextDouble();
+    
+    System.out.print( "Quantity in Stock: " );
+    int quantity = sc.nextInt(  );
+
+    Product product = new Product( "TV", 1000, 3 );
+
+
+    System.out.println( product.toString(  ) );
+    System.out.print( "Enter the number of products to be addes in stock: " );
+
+    quantity = sc.nextInt(  );
+    product.addProducts( quantity );
+   
+    System.out.println( product.toString(  ) );
+
+
+    System.out.println( product.toString(  ) );
+    System.out.print( "Enter the number of products to be remove from stock: " );
+
+    quantity = sc.nextInt(  );
+    product.removeProducts( quantity );
+
+    System.out.println( product.toString(  ) );
+
+    sc.close();
+    }
+
+}
+
--- Poo/Static Members/Exemple01.java
+++ Poo/Static Members/Exemple01.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Exemple01 {
 
-    public static void main(String[] args) {
+    public static void main( String[ ] args ) {
         
         Scanner sc = new Scanner( System.in );
 
@@ -13,12 +13,11 @@ public class Exemple01 {
 
         double v = Calculator.volume( radius );
 
-
         System.out.printf( "The Circumference's value: %.2f", c );
         System.out.printf( "\nThe Volume's value : %.2f", v );
         System.out.printf( "\nThe PI has value: %.2f", Calculator.PI );
 
-        sc.close();
+        sc.close(  );
 
     }
 

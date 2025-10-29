ebbb6de7e6912c4af83bb14329e965f2696da472
Danieldiaxf <danieldias0312@gmail.com>
2025-10-29 16:28:29 -0300

    comments added

--- Poo/Conductor/Vector/Problems/entities/People.java
+++ Poo/Conductor/Vector/Problems/entities/People.java
@@ -1,19 +1,22 @@
 package Conductor.Vector.Problems.entities;
 
+// Declaração da classe pública chamada 'People'
 public class People {
-    
+    // Declara um atributo (variável) privado do tipo String, chamado 'name'
     private String name;
+    // Declara um atributo privado do tipo int (número inteiro), chamado 'age'
     private int age;
+    // Declara um atributo privado do tipo double (número decimal), chamado 'height'
     private double height;
 
-
+    // Construtor da classe 'People'
     public People( String name, int age, double height ){
         this.name = name;
         this.age = age;
         this.height = height;
     }
 
-
+    // Get and Setters
     public String getName( ) {
         return name;
     }
@@ -44,6 +47,7 @@ public class People {
     }
 
 
+    // To String
     @Override
     public String toString(  ) {
         return "Name: " + name +

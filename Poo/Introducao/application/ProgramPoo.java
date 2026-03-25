528e0e15073c3d07aee121969835cd77eee484b7
Danieldiaxf <danieldias0312@gmail.com>
2026-03-25 16:46:41 -0300

    Feat: Usando POO

---
+++ Poo/Introducao/application/ProgramPoo.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.Scanner;
+import entities.TrianguloPoo;
+
+public class ProgramPoo {
+
+    public static void main( String[ ] args ) {
+
+        Scanner sc = new Scanner( System.in );
+
+        TrianguloPoo triangulo = new TrianguloPoo(  );
+
+        triangulo.setL1(3);
+        triangulo.setL2(4);
+        triangulo.setL3(5);
+
+        System.out.println( triangulo.area( ) );
+
+    }
+}
--- Poo/Introducao/entities/TrianguloPoo.java
+++ Poo/Introducao/entities/TrianguloPoo.java
@@ -1,16 +1,45 @@
 package entities;
 public class TrianguloPoo {
 
-    public double l1;
-    public double l2;
-    public double l3;
+    private double l1;
+    private double l2;
+    private double l3;
 
-    public double p( double l1, double l2, double l3 ) {
+    public TrianguloPoo() {
+    }   
+
+    public TrianguloPoo(double l1, double l2, double l3) {
         this.l1 = l1;
         this.l2 = l2;
         this.l3 = l3;
-        return ( l1 + l2 + l3 ) / 2;
     }
 
-    
+    public double getL1() {
+        return l1;
+    }
+    public void setL1(double l1) {
+        this.l1 = l1;
+    }
+
+    public double getL2() {
+        return l2;
+    }
+    public void setL2(double l2) {
+        this.l2 = l2;
+    }
+
+    public double getL3() {
+        return l3;
+    }
+    public void setL3(double l3) {
+        this.l3 = l3;
+    }
+
+    public Double area (  ) {
+        double p = ( l1 + l2 + l3 ) / 2.0; 
+        double result = Math.sqrt( p * ( p - l1 ) * ( p - l2 ) * ( p - l3 ) );
+        return result;
+    }
+
+     
 }

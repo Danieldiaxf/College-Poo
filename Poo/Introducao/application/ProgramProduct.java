f31aff81ae22737f50932c5348b50eb95b33355f
Danieldiaxf <danieldias0312@gmail.com>
2026-03-26 13:38:54 -0300

    SEC 8 AL 6

---
+++ Poo/Introducao/application/ProgramProduct.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.util.Scanner;
+
+import Constructor.entities.Product;
+
+public class ProgramProduct {
+    
+    public static void main( String[] args ){
+
+        Scanner sc = new Scanner( System.in );
+
+        Product product = new Product(  );
+
+        System.out.print( "Insira o nome do produto: " );
+        product.name = sc.nextLine(  );
+
+        System.out.print( "Insira a quantidade do produto: " );
+        product.quantity = sc.nextInt(  );
+
+        System.out.print( "Insira o preço do produto: " );
+        product.price = sc.nextDouble(  );
+        System.out.println( );
+        
+        System.out.println( "### Dados do Produto ###" );
+
+        System.out.printf( "Name: %s\n", product.name );
+        System.out.printf( "Quantity: %d\n", product.quantity );
+        System.out.printf( "Name: %.2f\n", product.price );
+        System.out.println( "Total: " + product.totalValueInStock( ) );
+        System.out.println(  );
+
+        System.out.print( "Produtos a serem adicionados:" );
+        int x = sc.nextInt( );
+        product.addProducts(x);
+        System.out.print( "Produtos a serem removidos:" );
+        int y = sc.nextInt( );
+        product.removeProducts(y);
+        System.out.println(  );
+
+        System.out.println( "### Dados Atualizados do Produto ###" );
+        System.out.printf( "Name: %s\n", product.name );
+        System.out.printf( "Quantity: %d\n", product.quantity );
+        System.out.printf( "Name: %.2f\n", product.price );
+        System.out.println( "Total: " + product.totalValueInStock( ) );
+
+        sc.close(  );
+    }
+
+
+}
---
+++ Poo/Introducao/entities/Product.java
@@ -0,0 +1,37 @@
+package entities;
+
+public class Product {
+    
+    public String name;
+    public Double price;
+    public int quantity;
+
+    public Product( ) {
+    }
+
+    public Product( String name, Double price, int quantity ) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    
+    public double totalValueInStock( ) {
+        return price * quantity;
+    }
+
+
+    public void addProducts( int x ) {
+        quantity += x;
+    }
+
+
+    public void removeProducts( int x ) {
+        quantity -=  x;
+    }
+
+
+
+
+
+}
